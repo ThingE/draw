9316460138f281612dae0e2a73a8814f79e0c656
ThingE <690509524@qq.com>
2023-08-16 10:12:26 +0800

    aaa

---
+++ no1/src/com/lxy/aaa.java
@@ -0,0 +1,14 @@
+package com.lxy;
+
+
+/**
+ * @author：ThingE
+ * @date：2023/8/16
+ * @version：v1.0
+ */
+
+public class aaa {
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+	}
+}
